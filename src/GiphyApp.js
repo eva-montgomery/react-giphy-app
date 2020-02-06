df93dd12acab2e507803e3dd89f400c6d696c795
eva montgomery <eva.m.montgomery@gmail.com>
2020-02-06 13:13:21 -0500

    gets random Gipgy indeasd if array of Giphies

--- src/GiphyApp.js
+++ src/GiphyApp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import GiphyImage from './GiphyImage';
 
-const giphyURL = 'https://api.giphy.com/v1/gifs/search?api_key=w7f3GOjdx1tXBzmlqn5M2RgfvbkzC6vL&q=laser&limit=25&offset=0&rating=G&lang=en';
+const giphyURL = 'https://api.giphy.com/v1/gifs/random?api_key=w7f3GOjdx1tXBzmlqn5M2RgfvbkzC6vL&tag=cat&rating=G';
 
 
 class GiphyApp extends React.Component {
@@ -18,6 +18,7 @@ class GiphyApp extends React.Component {
         return (
             <div>
                <button onClick={this._getGiphy}> 🌈 </button>
+               <br></br>
             {
                 this.state.giphies.map(giphy => (
                     <GiphyImage giphy={giphy} />
@@ -33,12 +34,12 @@ _getGiphy = () => {
 
     axios.get(giphyURL)
         .then(response => {
-            console.log(response.data.data[0].images.downsized_large)
+            console.log(response.data.data.images.downsized_large)
             // console.log('I clicked a button');
             this.setState({
                 giphies: [
+                    response.data.data.images.downsized_large,
                     ...this.state.giphies,
-                    response.data.data[0].images.downsized_large
                 ]
                 });
         })
